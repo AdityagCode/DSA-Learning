13d2ac79ac9c92a30e7c21ad49df601953150335
Aditya Gaikwad <125537461+AdityagCode@users.noreply.github.com>
2026-07-10 17:43:53 +0530

    Time: 1 ms (99.84%), Space: 47.8 MB (38.11%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for(int i: nums){
+               if(i!=0){
+                    nums[index] = i;
+                    index++;
+               }
+        }
+        for(int i=index;i<nums.length;i++){
+            nums[i] = 0;
+        }
+    }
+
+}
